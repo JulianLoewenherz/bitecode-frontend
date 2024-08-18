6e0245129044f8265c4160a7cbaac3f0e5775c15
Julian Loewenherz <164665193+JulianLoewenherz@users.noreply.github.com>
2024-08-18 07:01:52 -0700

    worked on mission.js and added mission.css
    added flexbox to mission page and styling as well

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -1,7 +1,28 @@
 import React from "react";
+import './Mission.css'; // Import the CSS file
 
 const Mission = () => {
-  return <div>Mission</div>;
+  return (
+    <>
+      <div className="mission-title">Mission</div>
+      
+      <div className="row">
+        <div className="mission1">
+          <h2 className="vision-header">Our Vision</h2>
+          <p className="mission-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          </p>
+        </div>
+        <div className="mission2">
+          <img 
+            src="https://via.placeholder.com/300" 
+            alt="Placeholder" 
+            className="mission-image"
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Mission;
