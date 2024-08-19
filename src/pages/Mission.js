170e5a245a649193103db9721f0f18845e049428
Julian Loewenherz <164665193+JulianLoewenherz@users.noreply.github.com>
2024-08-19 19:09:12 -0400

    made changes to mission.js and mission.cs
    adjusted image sizing and padding and formatting of mission.js

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -1,24 +1,31 @@
 import React from "react";
-import './Mission.css'; // Import the CSS file
+import './Mission.css'; 
+import Splash from "../components/Splash";
+import Logo from "../components/Logo";
 
 const Mission = () => {
   return (
     <>
-      <div className="mission-title">Mission</div>
+      <Logo color="white" />
+      <Splash text="Our Mission" /> 
       
-      <div className="row">
-        <div className="mission1">
-          <h2 className="vision-header">Our Vision</h2>
-          <p className="mission-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          </p>
-        </div>
-        <div className="mission2">
-          <img 
-            src="https://via.placeholder.com/300" 
-            alt="Placeholder" 
-            className="mission-image"
-          />
+      <div className="mission-content">
+        <div className="mission-title">Mission</div>
+        
+        <div className="row">
+          <div className="mission1">
+            <h2 className="vision-header">Our Vision</h2>
+            <p className="mission-text">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+            </p>
+          </div>
+          <div className="mission2">
+            <img 
+              src="https://assets.entrepreneur.com/content/3x2/2000/20190506145520-GettyImages-1091916996.jpeg" 
+              alt="Placeholder" 
+              className="mission-image"
+            />
+          </div>
         </div>
       </div>
     </>
